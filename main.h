b794cc299a4d274bcafc9271d571a1bea121ef46
SafyaAbouelfadel <safia.abf@gmail.com>
2024-07-31 07:30:53 -0700

    added main.h file

---
+++ main.h
@@ -0,0 +1,110 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#define DATA_BUFF_SIZE 1024
+#define DATA_CLEAR -1
+
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+#define NULL_STRING "(null)"
+
+#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+/**
+ * struct parameters - Holds formatting options
+ *
+ * @unsign: Flag for unsigned values
+ * @plusFlag: Flag for plus sign
+ * @spaceFlag: Flag for space
+ * @hashtagFlag: Flag for hashtag
+ * @zeroFlag: Flag for zero padding
+ * @minusFlag: Flag for left alignment
+ *
+ * @width: Field width
+ * @precision: Field precision
+ *
+ * @hModifier: Flag for 'h' modifier
+ * @lModifier: Flag for 'l' modifier
+ */
+
+typedef struct {
+    bool unsign;    /* u specifier */
+    bool left_align;    /* '-' flag*/
+    bool zero_pad;  /* '0' flag */
+    bool alternate_form;    /* # flag */
+    bool force_sign;    /* + flag */
+    bool space_flag;    /* ' ' flag */
+    unsigned int width;
+    unsigned int precision;
+    unsigned int short_modif;
+    unsigned int long_modif;
+} parameter_t;
+
+/* structure specifier */
+
+typedef struct specifier
+{
+	char *specifier;
+	int (*f)(va_list, parameter_t *);
+} specifier_t;
+
+int _printf(const char *format, ...);
+
+/* numerals*/
+int _is_digit(int num);
+char *converter(long int number, int base, int flags, parameter_t *params);
+int output_unsigned_int(va_list args, parameter_t *params);
+int format_address(va_list args, parameter_t *params);
+
+/* print_digit */
+int str_length(int num);
+int format_number(char *string, parameter_t *params);
+int _string_length(char *c);
+int format_number_shifted_right(char *string, parameter_t *params);
+int format_number_shifted_left(char *string, parameter_t *params);
+
+/* output_functions*/
+int output_character(va_list args, parameter_t *params);
+int output_integer(va_list args, parameter_t *params);
+int output_string(va_list args, parameter_t *params);
+int output_percentage(va_list args, parameter_t *params);
+int output_S(va_list args, parameter_t *params);
+
+/* strings*/
+int _putchar(char c);
+int _puts(char *s);
+
+/* format_options*/
+void initial_params(parameter_t *params, va_list arg);
+char *get_precision(char *s, parameter_t *params, va_list ap);
+char *get_width(char *s, parameter_t *params, va_list ap);
+int *get_flag(char *s, parameter_t *params);
+int get_length_modifiers(char *s, parameter_t *params);
+
+/* function*/
+int output_range(char *begin, char *end, char*expect);
+int output_reverse(va_list ap, parameter_t params);
+int output_rot13(va_list ap, parameter_t params);
+
+/* number_conversion*/
+int format_hex(va_list args, parameter_t *params);
+int format_HEX(va_list args, parameter_t *params);
+int format_binary(va_list args, parameter_t *params);
+int format_octal_value(va_list args, parameter_t *params);
+
+/* function(need a name)*/ 
+int (*get_specifier(char *s))(va_list ap, parameter_t *params);
+int fetch_output_function(char *str, va_list arg, parameter_t *params);
+
+/* print*/
+int print_char(va_list *str, parameter_t *params);
+
+#endif /* MAIN_H */
